3d3dde6d9985da23d1804fdc5a3535ea55bb6f0d
jhon mc <kgbxblackx@gmail.com>
2021-05-13 00:15:33 -0500

    Se crean los metodos en el controlador para actualizar, borrar la informacion de un carro.
    Tambien se crea un handler para manejar los errores de un vehiculo no encontrado a la hora de querer actualizarlo.

--- src/main/java/com/sura/escenario4/controller/InformacionCarroController.java
+++ src/main/java/com/sura/escenario4/controller/InformacionCarroController.java
@@ -3,11 +3,10 @@ package com.sura.escenario4.controller;
 import com.sura.escenario4.model.InformacionCarro;
 import com.sura.escenario4.service.InformacionCarroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.Locale;
 
 @RestController
 @RequestMapping("/carinfo")
@@ -34,6 +33,25 @@ public class InformacionCarroController {
     @PostMapping
     Mono<InformacionCarro> save(@RequestBody InformacionCarro carro){
         return carroService.save(carro);
+    }
+
+    @PutMapping
+    Mono<InformacionCarro>update(@RequestBody InformacionCarro carro) {
+        return carroService.findByPlacaLike(carro.getPlaca())
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("No car info found with this placa")))
+                .flatMap(carro1 -> carroService.save(carro))
+                .single();
+    }
+
+    @DeleteMapping("/{placa}")
+    Mono<Void> delete(@PathVariable("placa") String placa){
+        return carroService.delete(placa.toUpperCase());
+    }
+
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "There is not element stored with that placa")
+    @ExceptionHandler({IllegalArgumentException.class})
+    public void handelIlegalArgumentExeption(){
     }
 
 }
