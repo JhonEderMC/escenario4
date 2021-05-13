962dfecac6a7d287725da33567b35de387cbdf05
jhon mc <kgbxblackx@gmail.com>
2021-05-12 19:19:27 -0500

    Se crea una interfaz para definir el comportaminto que tendra el servicio.

---
+++ src/main/java/com/sura/escenario4/service/InformacionCarroServiceInterface.java
@@ -0,0 +1,10 @@
+package com.sura.escenario4.service;
+
+import com.sura.escenario4.model.InformacionCarro;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface InformacionCarroServiceInterface {
+    Mono<InformacionCarro> save(InformacionCarro carro);
+    Flux<InformacionCarro> findByPlacaLike(String placa);
+}
